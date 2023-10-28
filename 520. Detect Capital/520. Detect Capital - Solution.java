d28c412aa8c48236f0ed3d95373c3fecf0e5981f
HelloWorldDC <githubdc9999@gmail.com>
2023-10-28 22:46:53 +0700

    Upload Exercise 520 Detect Capital

---
+++ 520. Detect Capital/520. Detect Capital - Solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+	public static boolean detectCapitalUse(String word) {
+		if(word.length() < 2) {
+			return true;
+		}
+		if(Character.isUpperCase(word.charAt(0)) && Character.isUpperCase(word.charAt(1))) {
+			for (int i = 2; i < word.length(); i++) {
+				if(Character.isLowerCase(word.charAt(i))) {
+					return false;
+				}
+			}
+		}
+		else if(Character.isLowerCase(word.charAt(0))) {
+			for (int i = 1; i < word.length(); i++) {
+				if(Character.isUpperCase(word.charAt(i))) {
+					return false;
+				}
+			}
+		}
+		else if(Character.isUpperCase(word.charAt(0))) {
+			for (int i = 1; i < word.length(); i++) {
+				if(Character.isUpperCase(word.charAt(i))) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(detectCapitalUse("Google"));
+	}
+}
---
+++ 520. Detect Capital/README.md
@@ -0,0 +1,34 @@
+# 520. Detect Capital
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+<li>All letters in this word are capitals, like <code>"USA"</code>.</li>
+<li>All letters in this word are not capitals, like <code>"leetcode"</code>.</li>
+<li>Only the first letter in this word is capital, like <code>"Google"</code>.</li>
+
+<br/>
+
+Given a string `word`, return `true` if the usage of capitals in it is right.
+
+ 
+
+
+**Example 1:**
+
+>**Input:** word = "USA"  
+**Output:** true  
+
+**Example 2:**
+
+>**Input:** word = "FlaG"  
+**Output:** false  
+ 
+
+**Constraints:**
+
+<li><code>1 &lt;= word.length &lt;= 100</code></li>
+<li><code>word</code> consists of lowercase and uppercase English letters.</li>
+
+<br/>
+
+**Exercise on Website:** https://leetcode.com/problems/detect-capital
