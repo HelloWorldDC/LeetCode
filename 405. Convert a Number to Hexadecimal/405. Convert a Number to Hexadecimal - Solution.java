dfcaf54be32453d1c013b70004c437af12f6e0d1
HelloWorldDC <githubdc9999@gmail.com>
2023-10-28 22:45:20 +0700

    Upload Exercise 405 Convert a Number to Hexadecimal

---
+++ 405. Convert a Number to Hexadecimal/405. Convert a Number to Hexadecimal - Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+	public static String toHex(int num) {
+		
+		return Integer.toHexString(num);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(toHex(-1));
+	}
+}
