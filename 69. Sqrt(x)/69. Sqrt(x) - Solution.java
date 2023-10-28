9153e8a20d89af7b449a0cf6a4df4dd26d106ba1
HelloWorldDC <githubdc9999@gmail.com>
2023-10-28 22:47:47 +0700

    Upload Exercise 69 Sqrt(x)

---
+++ 69. Sqrt(x)/69. Sqrt(x) - Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+	public static int mySqrt(int x) {
+		int result = (int)Math.sqrt(x);
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(mySqrt(8));
+	}
+}
